9dcffc8ebd6441ed3966da5e64626de1958789ee
Hjacobsen72 <trexinator72@gmail.com>
2025-12-31 14:30:40 -0800

    Update +page.svelte

--- src/routes/constitution/+page.svelte
+++ src/routes/constitution/+page.svelte
@@ -102,7 +102,7 @@
     
     <h3 class="subSectionHeading" bind:this={oneOne}>1.1 Positional Breakdown</h3>
     
-    <p>28 Total Players</p>
+    <p>14 Total Players</p>
     
     <p class="underscore">Starters</p>
     <ul>
